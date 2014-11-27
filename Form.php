91aeccd0c4cf144f57c45e5c3d110f76bd6f0c2a
Kirill Brusenskiy <kirill@brusenskiy.ru>
2014-11-27 09:54:55 +0300

    Add method to set order of form elements.

--- Form.php
+++ Form.php
@@ -158,6 +158,23 @@ class Form extends \StdClass
         return $this->_elements[$_name];
     }
 
+    public function orderElementAfter($_name, $_after)
+    {
+        $move = $this->_elements[$_name];
+        $tmp = [];
+
+        foreach ($this->_elements as $name => $element) {
+            if ($name == $_name) continue;
+
+            $tmp[$name] = $element;
+
+            if ($name == $_after)
+                $tmp[$_name] = $move;
+        }
+
+        $this->_elements = $tmp;
+    }
+
     public function delete($_name)
     {
         $name = $this->computeInnerName($_name);
